3c4aa64b6ff6dbf6f6e51cc5b3b8e7c1328c6875
sabbir <sabbirmahmud4455@gmail.com>
2023-08-09 22:18:55 +0600

    install and setup laravel, inertia, react and tailwind

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Test = () => {
+    return (
+        <h1 className=' text-red-500 text-lg'>This is test component</h1>
+    )
+}
+
+export default Test
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return Inertia::render('Test'); // This will get component Test.jsx from the resources/js/Pages/Test.jsx
+});
